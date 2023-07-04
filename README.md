fe4235d85383cbc98649b5b243dccc2adbf75ebd
Ryan Pizani <110544301+rypizani@users.noreply.github.com>
2023-07-04 14:48:57 -0300

    kkk

--- README.md
+++
@@ -1,3 +0,0 @@
-# React SPA para Portal Noivas
-
-Feito com React e Redux.
--- src/apis/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://vps43892.publiccloud.com.br",
-    timeout: 20000
-});
-
-export default api;
